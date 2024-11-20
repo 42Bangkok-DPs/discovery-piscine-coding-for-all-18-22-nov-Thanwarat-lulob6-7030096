2751fa8be769d75dee04ffb15acea8c787a04fb9
Thanwarat-lulob6-7030096 <67030096@kmitl.ac.th>
2024-11-20 19:33:33 +0000

    main

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-F_name = input(f"what's your first name")
-L_name = input(f"and your last name")
+F_name = input(f"what's your first name : ")
+L_name = input(f"and your last name : ")
 print(F_name,L_name)
