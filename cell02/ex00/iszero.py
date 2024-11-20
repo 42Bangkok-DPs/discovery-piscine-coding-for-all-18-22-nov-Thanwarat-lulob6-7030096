58b8b6aa4ce9759271c2990ae1eaea2481be707a
Thanwarat-lulob6-7030096 <67030096@kmitl.ac.th>
2024-11-20 21:59:08 +0000

    main

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+while True:
+    number = float(input("Enter a number: "))
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
