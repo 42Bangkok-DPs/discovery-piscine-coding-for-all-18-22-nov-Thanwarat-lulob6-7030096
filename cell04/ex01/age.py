18ddf23c1d06a56eb1ad259d495f6996e5462ff6
Thanwarat-lulob6-7030096 <67030096@kmitl.ac.th>
2024-11-21 11:59:17 +0000

    main

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,3 +1,5 @@
-user_input = input("Give me a string: ")
-swapped_case = user_input.swapcase()
-print(swapped_case)
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
