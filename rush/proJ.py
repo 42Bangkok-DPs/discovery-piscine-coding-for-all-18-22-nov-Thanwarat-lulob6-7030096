57a02fef096bddcb1648fc6793925bef3ae12870
Thanwarat-lulob6-7030096 <67030096@kmitl.ac.th>
2024-11-21 09:09:48 +0000

    main

--- rush/proJ.py
+++ rush/proJ.py
@@ -7,8 +7,7 @@ def is_king_in_check(board):
         return None
 
     def is_attacked_by_pawn(king_row, king_col):
-        # Check for pawns that can attack the king (from below)
-        directions = [(-1, -1), (-1, 1)]  # Diagonal attacks
+        directions = [(-1, -1), (-1, 1)] 
         for dr, dc in directions:
             nr, nc = king_row + dr, king_col + dc
             if 0 <= nr < size and 0 <= nc < size and board[nr][nc] == 'P':
